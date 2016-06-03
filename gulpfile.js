663099f3e417f575835e0b7c031bfe1cec94138e
kaflan <kaf_90@mail.ru>
2016-06-03 10:53:18 +0300

    livereload work

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 var gulp = require('gulp');
 var server = require('gulp-server-livereload');
 
-gulp.task('server', function() {
+gulp.task('serve', function() {
   gulp.src('app')
     .pipe(server({
       livereload: true,
-      directoryListing: true,
+      directoryListing: false,
       open: true
     }));
 });
