2973eb59026c8fcac29d3717bf658c7e539b6a05
kaflan <kaf_90@mail.ru>
2016-06-03 11:55:13 +0300

    add functions  togle

--- app/index.js
+++ app/index.js
@@ -1,2 +1,14 @@
 var JQ = jQuery;
-console.log(JQ);
+var hour = JQ('#hour').val();
+var minute = JQ('#minute').val();
+var second = JQ('#second').val();
+var inputs = JQ('.inputs');
+var run = JQ('#run').click(function () {
+ inputs.toggle();
+});
+var pause = JQ('#pause').click(function(){
+
+});
+var stop = JQ('#stop').click(function() {
+  inputs.toggle();
+});
